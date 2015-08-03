a1bdb1bce3fec420d41b28abb7b0fd4bbe5696a2
gonzalo <gonzalo@ebizmarts.com>
2015-08-03 12:00:41 -0300

    add Hint to the admin

---
+++ Block/Adminhtml/System/Config/Fieldset/Hint.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Author: info@ebizmarts.com
+ * Date: 8/1/15
+ * Time: 3:04 AM
+ * File: Hint.php
+ * Module: magento2-mandrill
+ */
+namespace Ebizmarts\Mandrill\Block\Adminhtml\System\Config\Fieldset;
+
+class Hint extends \Magento\Backend\Block\Template implements \Magento\Framework\Data\Form\Element\Renderer\RendererInterface
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Ebizmarts_Mandrill::system/config/fieldset/hint.phtml';
+    /**
+     * @var \Magento\Framework\App\ProductMetadataInterface
+     */
+    protected $_metaData;
+    /**
+     * @var \Magento\Framework\Module\ModuleList\Loader
+     */
+    protected $_loader;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\App\ProductMetadataInterface $productMetaData
+     * @param \Magento\Framework\Module\ModuleList\Loader $loader
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\App\ProductMetadataInterface $productMetaData,
+        \Magento\Framework\Module\ModuleList\Loader $loader,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->_metaData = $productMetaData;
+        $this->_loader = $loader;
+    }
+
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return mixed
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return $this->toHtml();
+    }
+
+    /**
+     * @return bool
+     */
+    public function mandrillEnabled()
+    {
+        return false;
+    }
+    public function getPxParams()
+    {
+        $modules = $this->_loader->load();
+        $v = "";
+        if(isset($modules['Ebizmarts_Mandrill']))
+        {
+            $v =$modules['Ebizmarts_Mandrill']['setup_version'];
+        }
+        $extension = "Mandrill;{$v}";
+        $mageEdition = $this->_metaData->getEdition();
+        switch($mageEdition)
+        {
+            case 'Community':
+                $mageEdition = 'CE';
+                break;
+            case 'Enterprise':
+                $mageEdition = 'EE';
+                break;
+        }
+        $mageVersion = $this->_metaData->getVersion();
+        $mage = "Magento {$mageEdition};{$mageVersion}";
+        $hash = md5($extension . '_' . $mage . '_' . $extension);
+        return "ext=$extension&mage={$mage}&ctrl={$hash}";
+
+    }
+    public function getVersion()
+    {
+        $modules = $this->_loader->load();
+        $v = "";
+        if(isset($modules['Ebizmarts_Mandrill']))
+        {
+            $v =$modules['Ebizmarts_Mandrill']['setup_version'];
+        }
+        return $v;
+    }
+
+}
