fc5a21dc3a16029974ed9ca4de1c50d60cbc6ba6
gonzalo <gonzalo@ebizmarts.com>
2016-02-24 12:34:25 -0300

    add registration

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Mandrill Magento Component
+ *
+ * @category Ebizmarts
+ * @package Mandrill
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 2/24/16 1:33 PM
+ * @file: registration.php
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Ebizmarts_Mandrill',
+        __DIR__
+    );
