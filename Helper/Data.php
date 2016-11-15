313577976982c594bb479ee7c0b10ef1a37b4776
Santiago <santiago@ebizmarts.com>
2016-11-15 14:49:48 -0300

    Added missing functions.

--- Helper/Data.php
+++ Helper/Data.php
@@ -33,6 +33,10 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         parent::__construct($context);
     }
 
+    /**
+     * @param null $store
+     * @return mixed
+     */
     public function getApiKey($store = null)
     {
         return $this->scopeConfig->getValue(self::XML_PATH_APIKEY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
@@ -41,6 +45,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         return $this->scopeConfig->getValue(self::XML_PATH_ACTIVE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
     }
+
     /**
      * @codeCoverageIgnore
      */
@@ -48,11 +53,87 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         $this->_logger->info($msg);
     }
+
+    /**
+     * @return mixed
+     */
     public function getTestSender()
     {
         return $this->scopeConfig->getValue(
             'checkout/payment_failed/identity',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
+    }
+
+    /**
+     * @param $mailType
+     * @param $mail
+     * @param $name
+     * @param $couponCode
+     * @param $storeId
+     */
+    public function saveMail($mailType,$mail,$name,$couponCode,$storeId)
+    {
+        if ($couponCode != '') {
+            $coupon = $this->_objectManager->create('Magento\SalesRule\Model\Coupon')->loadByCode($couponCode);
+            $rule = $this->_objectManager->create('Magento\SalesRule\Model\Rule')->load($coupon->getRuleId());
+            $couponAmount = $rule->getDiscountAmount();
+            switch ($rule->getSimpleAction()) {
+                case 'cart_fixed':
+                    $couponType = 1;
+                    break;
+                case 'by_percent':
+                    $couponType = 2;
+                    break;
+            }
+        } else {
+            $couponType = 0;
+            $couponAmount = 0;
+        }
+        $sent = $this->_objectManager->create('Ebizmarts\Mandrill\Model\Mailsent');
+        $date = $this->_objectManager->create('\Magento\Framework\Stdlib\DateTime\DateTime');
+        $sent->setMailType($mailType)
+            ->setStoreId($storeId)
+            ->setCustomerEmail($mail)
+            ->setCustomerName($name)
+            ->setCouponNumber($couponCode)
+            ->setCouponType($couponType)
+            ->setCouponAmount($couponAmount)
+            ->setSentAt($date->gmtDate())
+            ->save();
+    }
+    public function isSubscribed($email, $list, $storeId)
+    {
+        $subscribed = $this->_subscribed;
+        $isSubscribed = $subscribed[$storeId][$list][$email];
+        if(!isset($isSubscribed)) {
+            $this->log('not cached');
+            return $this->_checkSubscription($email, $list, $storeId);
+        }else{
+            $this->log('cached');
+            return $isSubscribed;
+        }
+    }
+
+    /**
+     * @param $email
+     * @param $list
+     * @param $storeId
+     * @return bool
+     */
+    private function _checkSubscription($email, $list, $storeId){
+        $collection = $this->_objectManager->create('\Ebizmarts\Mandrill\Model\Unsubscribe')->getCollection();
+        $collection->addFieldToFilter('main_table.email', array('eq' => $email))
+            ->addFieldToFilter('main_table.list', array('eq' => $list))
+            ->addFieldToFilter('main_table.store_id', array('eq' => $storeId));
+        if ($collection->getSize() == 0) {
+            $this->_subscribed[$storeId][$list][$email] = 'true';
+            $this->log('true');
+            return true;
+        } else {
+            $this->_subscribed[$storeId][$list][$email] = 'false';
+            $this->log('false');
+            return false;
+        }
     }
 }
