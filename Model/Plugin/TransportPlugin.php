815a554350dfadb3b08c53b801b7ac835ebbc2cf
centerax <pablosbenitez@gmail.com>
2017-04-24 17:51:19 -0300

    add transport plugin

---
+++ Model/Plugin/TransportPlugin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ebizmarts\Mandrill\Model\Plugin;
+
+
+class TransportPlugin
+{
+    private $mandrillTransportFactory;
+
+    private $mandrillHelper;
+
+    public function __construct(
+        \Ebizmarts\Mandrill\Helper\Data $mandrillHelper,
+        \Ebizmarts\Mandrill\Model\TransportFactory $mandrillTransportFactory
+    )
+    {
+        $this->mandrillHelper = $mandrillHelper;
+        $this->mandrillTransportFactory = $mandrillTransportFactory;
+    }
+
+    public function aroundSendMessage(\Magento\Framework\Mail\Transport $mailTransport, callable $proceed)
+    {
+        if ($this->isMandrillEnabled() === false) {
+            $proceed();
+        } else {
+            /** @var \Ebizmarts\Mandrill\Model\Transport $mandrillTransport */
+            $mandrillTransport = $this->mandrillTransportFactory->create();
+            $mandrillTransport->sendMessage();
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function isMandrillEnabled()
+    {
+        return (1 === (int)$this->mandrillHelper->isActive());
+    }
+
+}
