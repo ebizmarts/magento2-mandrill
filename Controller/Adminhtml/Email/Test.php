796474f89daced6d710623f2e9479f55679bfb43
Alexey Arendarenko <alexeya@ven.com>
2019-03-18 11:59:14 +0200

    Test controller changes
    Add storeManager class to get default storeId instead of defining it's in code.

--- Controller/Adminhtml/Email/Test.php
+++ Controller/Adminhtml/Email/Test.php
@@ -11,6 +11,7 @@
 namespace Ebizmarts\Mandrill\Controller\Adminhtml\Email;
 
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Test extends \Magento\Backend\App\Action
 {
@@ -23,21 +24,27 @@ class Test extends \Magento\Backend\App\Action
      */
     protected $_helper;
 
+    /** @var StoreManagerInterface */
+    public $storeManager;
+
     /**
      * Test constructor.
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
      * @param \Ebizmarts\Mandrill\Helper\Data $helper
+     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
         \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
-        \Ebizmarts\Mandrill\Helper\Data $helper
+        \Ebizmarts\Mandrill\Helper\Data $helper,
+        StoreManagerInterface $storeManager
     ) {
-    
+
         parent::__construct($context);
         $this->_transportBuilder = $transportBuilder;
         $this->_helper = $helper;
+        $this->storeManager = $storeManager;
     }
 
     public function execute()
@@ -48,7 +55,9 @@ class Test extends \Magento\Backend\App\Action
         $this->_transportBuilder->setFrom($this->_helper->getTestSender());
         $this->_transportBuilder->addTo($email);
         $this->_transportBuilder->setTemplateVars([]);
-        $this->_transportBuilder->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => 1]);
+        $this->_transportBuilder->setTemplateOptions(
+            ['area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => $this->storeManager->getStore()->getId()]
+        );
         $transport = $this->_transportBuilder->getTransport();
         $transport->sendMessage();
 
